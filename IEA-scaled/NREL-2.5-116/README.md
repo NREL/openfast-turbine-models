ac0c5bd197bc7032c4b60f67c11922323fd2250f
Eliot Quon <eliot.quon@nrel.gov>
2020-11-25 14:25:37 -0700

    Update turbine design with peak shaving
    - peak shaving capability provided by https://github.com/WISDEM/WISDEM/commit/a14307767c57ec2f32fb78bfff2a6b1dfda6574d
    - updated wisdem_driver.py do all design steps

--- IEA-scaled/NREL-2.5-116/README.md
+++ IEA-scaled/NREL-2.5-116/README.md
@@ -1,15 +1,18 @@
 # NREL 2.5-116
 
+Design by [Eliot Quon](mailto:eliot.quon@nrel.gov) and Pietro Bortolotti
+
 | Design Variable | Value | Description |
 | --------------- | ----- | ----------- |
 | rated electrical power | 2.5 MW | specified |
 | diameter | 116 m | specified |
 | hub radius | 2 m | same as IEA-3.4-130 |
 | tower height | 80 m | specified |
-| tip-speed ratio | 9.0 | increased from IEA-3.4-130 (TSR=8.02) |
+| tip-speed ratio | 9.0 | estimate, increased from IEA-3.4-130 (TSR=8.02) |
 | cut-in wind speed | 3 m/s | specified |
 | rated wind speed | 10.5 m/s | target |
 | cut-out wind speed | 25 m/s | specified |
+| peak shaving | 25% | estimate |
 
 Design approach:
 
@@ -19,11 +22,12 @@ Design approach:
 * Perform twist and chord optimization for max AEP, with max chord and stall constraints
 * Perform spar cap optimization for min blade mass
 * Perform tower layer thickness optimization for min tower mass
-* *TODO*: Add peak shaving
 
 Reference data:
 
 * P, Cp, Ct for GE-2.5-120 from
   https://en.wind-turbine-models.com/turbines/310-ge-general-electric-ge-2.5-120
+  (accessed 2020-11-23)
 * P cross-referenced with
   https://www.thewindpower.net/turbine_en_592_ge-energy_2.5-120.php
+  (accessed 2020-11-23)
--- IEA-scaled/NREL-2.5-116/WISDEM/wisdem_driver.py
+++ IEA-scaled/NREL-2.5-116/WISDEM/wisdem_driver.py
@@ -8,47 +8,55 @@ import os, time, sys
 run_dir = './'
 fname_modeling_options = run_dir + "modeling_options.yaml"
 
+tt = time.time()
+
 # step 0: sanity check, initial IEA-3.4-130-RWT -- NO OPT
 fname_wt_input         = run_dir + "IEA-3.4-130-RWT.yaml"
 fname_analysis_options = run_dir + "analysis_options.0.yaml"
+print('STEP 0')
+wt_opt, modeling_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
 
 # step 1: updated turbine rating, rotor size -- NO OPT
-# - scaled x and z values of components.blade.outer_shape_bem.reference_axis,
-#   defined as id001, by factor of 116/130
+# - rated_power: 2500000.0 W
+# - rotor_diameter: 116 m
+# - hub_height: 80 m
+print('STEP 1')
 fname_wt_input         = run_dir + "NREL-2_5-116-step0.yaml"
 fname_analysis_options = run_dir + "analysis_options.1.yaml"
+wt_opt, modeling_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
 
-# step 2: updated turbine height -- NO OPT
-# - scaled values components.tower.outer_shape_bem.reference_axis.z,
-#   by factor of 80/108
+# step 2: unconstrained twist opt for AEP
+# - add stall constraint
+# - need to use downgraded openmdao: `conda install -c conda-forge openmdao=3.2.1`
+print('STEP 2')
 fname_wt_input         = run_dir + "NREL-2_5-116-step1.yaml"
 fname_analysis_options = run_dir + "analysis_options.2.yaml"
+wt_opt, modeling_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
 
 # step 3: unconstrained twist opt for AEP
-# - adjusted min/max twist to be in bounds
-# - downgraded openmdao: `conda install -c conda-forge openmdao=3.2.1`
+# - increase TSR to 9.0 (for smaller turbine)
+# - increased rotor speed range to 8-14 (IEA 3.4: 6.9-12.1)
+# - add stall and max-chord constraint
+# - peak thrust shaving: 75%
+print('STEP 3')
 fname_wt_input         = run_dir + "NREL-2_5-116-step2.yaml"
 fname_analysis_options = run_dir + "analysis_options.3.yaml"
+wt_opt, modeling_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
 
-# step 4: constrained twist+chord opt for AEP
-# - increase TSR to 9.0 (for smaller turbine)
-# - change rotor speed range to between 5-14 RPM (IEA 3.4: 6.9-12.1)
-# - add stall and max-chord constraint
+# step 4: constrained structural spar-cap opt for blade mass
+# - add tip deflection constraint
+print('STEP 4')
 fname_wt_input         = run_dir + "NREL-2_5-116-step3.yaml"
 fname_analysis_options = run_dir + "analysis_options.4.yaml"
+wt_opt, modeling_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
 
-# step 5: constrained structural opt for blade mass
-# - add tip deflection constraint
+# step 5: unconstrained structural wall-thickness opt for tower mass
+# - reduced max tower diameter to 4.0 m (land-based transport limitations)
+# - scale min tower diameter by 3.87/6 (from NREL 5MW RWT)
+# - lienar taper from mid height
+print('STEP 5')
 fname_wt_input         = run_dir + "NREL-2_5-116-step4.yaml"
 fname_analysis_options = run_dir + "analysis_options.5.yaml"
-
-# step 6: constrained structural opt for blade mass
-# - add tip deflection constraint
-fname_wt_input         = run_dir + "NREL-2_5-116-step5.yaml"
-fname_analysis_options = run_dir + "analysis_options.6.yaml"
-
-
-tt = time.time()
 wt_opt, modeling_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
 
 if MPI:
