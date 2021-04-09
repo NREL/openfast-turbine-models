f83bb4c48e133e54b36e6b315af698088f3bc4ec
Eliot Quon <eliot.quon@nrel.gov>
2021-04-08 21:46:04 -0600

    Create separate drivers for WEIS analysis levels 1/2

--- IEA-scaled/NREL-1.7-103/WEIS/weis_driver.2.py
+++ IEA-scaled/NREL-1.7-103/WEIS/weis_driver.2.py
@@ -10,9 +10,8 @@ fname_analysis_options = run_dir + "analysis_options_weis.yaml"
 
 
 tt = time.time()
-fname_modeling_options = run_dir + "modeling_options_weis.1.yaml"
-wt_opt, modeling_options, opt_options = run_weis(fname_wt_input, fname_modeling_options, fname_analysis_options)
 
+# Analysis level 2: Run power curve sweep
 fname_modeling_options = run_dir + "modeling_options_weis.2.yaml"
 wt_opt, modeling_options, opt_options = run_weis(fname_wt_input, fname_modeling_options, fname_analysis_options)
 
