a84b13aef5897eab19e8c7c7a3a9b08ff7ffd6c2
scottryn <scottryn@gmail.com>
2022-11-16 15:48:20 -0700

    Upload 1.715 and 1.79 WISDEM files.

---
+++ IEA-scaled/NREL-1.79-100/WISDEM/wisdem_driver.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# check environment
+echo 'Checking for WISDEM in environment'
+python -c 'from wisdem import run_wisdem'
+echo 'Checking for WEIS in environment'
+python -c 'from weis.glue_code.runWEIS import run_weis'
+
+# perform sequential optimizations
+Nsteps=`ls wisdem_driver.?.py | wc -l`
+for i in `seq 0 $((Nsteps-1))`; do
+    if [ -f "log.step$i" -a -d "outputs.$i" ]; then
+        echo "Step $i has already been run"
+    else
+        python wisdem_driver.${i}.py 2>&1 | tee log.step$i
+    fi
+    if [ "$i" == 0 ]; then
+        echo 'Difference reference and current turbine:'
+        diff outputs.0/refturb.yaml NREL-*.start.yaml > refturb.diff
+    fi
+done
+
+echo 'Done.'
