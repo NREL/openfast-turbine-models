b66904d30f1bc75bd4d38e3035947cdcef01a934
Eliot Quon <eliot.quon@nrel.gov>
2024-03-17 16:19:57 -0600

    Save opt approach with smoothing for later, might be useful for other geometries

--- IEA-scaled/NREL-2.8-127/20_WISDEM_combined_aerostruct_opt2/with_smoothing/blade_aerostruct_opt.1.py
+++ IEA-scaled/NREL-2.8-127/20_WISDEM_combined_aerostruct_opt2/with_smoothing/blade_aerostruct_opt.1.py
@@ -2,7 +2,6 @@ import os
 from wisdem import run_wisdem
 
 mydir = os.path.dirname(os.path.realpath(__file__))  # get path to this file
-#fname_wt_input         = os.path.join(mydir, "aerostruct.0/NREL-2.8-127.yaml")
 fname_wt_input         = os.path.join(mydir, "aerostruct.0/NREL-2.8-127_smooth_twist.yaml")
 fname_modeling_options = os.path.join(mydir, "modeling_options.yaml")
 fname_analysis_options = os.path.join(mydir, "analysis_options.1.yaml")
